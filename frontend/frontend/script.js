bca1d3c220c91849085134b36277dfb3684019a8
ambikesh587036 <ambikeshdwivedi957@gmail.com>
2026-03-06 18:36:59 +0530

    Update script.js

--- frontend/frontend/script.js
+++ frontend/frontend/script.js
@@ -1,5 +1,37 @@
-function addToCart(product){
+let cart = []
+
+function addToCart(product, price){
+
+cart.push({product, price})
 
 alert(product + " added to cart")
 
+updateCart()
+
 }
+
+function updateCart(){
+
+let cartList = document.getElementById("cart-items")
+
+let totalPrice = document.getElementById("total")
+
+cartList.innerHTML = ""
+
+let total = 0
+
+cart.forEach(item => {
+
+let li = document.createElement("li")
+
+li.innerText = item.product + " - ₹" + item.price
+
+cartList.appendChild(li)
+
+total += item.price
+
+})
+
+totalPrice.innerText = "Total: ₹" + total
+
+  }
